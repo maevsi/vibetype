87a65320d9d3dca26e32a5f7b65e44155363d706
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-03-11 12:54:43 +0100

    fix(style): correct layout width

--- src/app/layouts/default.vue
+++ src/app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex min-h-screen lg:h-screen 2xl:h-auto">
     <LayoutMenuSidebar class="hidden lg:block 2xl:hidden" />
-    <div class="flex flex-1 flex-col">
+    <div class="flex min-w-0 flex-1 flex-col">
       <div
         class="container mx-auto flex flex-1 shrink flex-col p-4 md:px-8 lg:block lg:overflow-y-auto"
         :data-is-loading="isLoading"
