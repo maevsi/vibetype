1a48de793948c215eec8e66d72d824a0a8ede8ca
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-04-02 22:37:35 +0200

    feat(flow): add welcome screen

---
+++ src/app/components/typography/TypographySubtitleMedium.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="cn('text-base font-bold', classProps)">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { HtmlHTMLAttributes } from 'vue'
+
+import { cn } from '@/utils/shadcn'
+
+const { class: classProps } = defineProps<{
+  class?: HtmlHTMLAttributes['class']
+}>()
+</script>
---
+++ src/app/layouts/plain.vue
@@ -0,0 +1,20 @@
+<template>
+  <main
+    :data-is-loading="isLoading"
+    data-testid="is-loading"
+    class="flex h-screen"
+  >
+    <slot />
+  </main>
+</template>
+
+<script setup lang="ts">
+const { $dayjs } = useNuxtApp()
+const { locale } = useI18n()
+$dayjs.locale(locale.value)
+
+const loadingId = Math.random()
+const loadingIds = useState(STATE_LOADING_IDS_NAME, () => [loadingId])
+const isLoading = computed(() => !!loadingIds.value.length)
+onMounted(() => loadingIds.value.splice(loadingIds.value.indexOf(loadingId), 1))
+</script>
