7a5f855cf49f69a55f9ae13a304585117488a46e
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-04-03 05:01:55 +0200

    refactor(legal): extract content component

--- src/app/pages/legal-notice/index.vue
+++ src/app/pages/legal-notice/index.vue
@@ -1,22 +1,8 @@
 <template>
-  <LayoutProse>
-    <ContentRenderer v-if="page" :value="page" />
-  </LayoutProse>
+  <Content v-model="title" path="legal-notice" />
 </template>
 
 <script setup lang="ts">
-const { locale } = useI18n()
-
-const { data: page } = await useAsyncData(() =>
-  queryCollection('content').path(`/${locale.value}/legal-notice`).first(),
-)
-
-if (!page) {
-  throw createError({
-    statusCode: 404,
-  })
-}
-
-// initialization
-useHeadDefault({ title: page.value?.title })
+const title = ref<string>()
+useHeadDefault({ title })
 </script>
