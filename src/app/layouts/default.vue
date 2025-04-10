4bbbbbd2a4e3550f6139ff37a9452193cd92d22c
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-04-10 09:42:49 +0200

    feat(style): simplify for layout

--- src/app/layouts/default.vue
+++ src/app/layouts/default.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="flex min-h-screen lg:h-screen 2xl:h-auto">
-    <LayoutMenuSidebar class="hidden lg:block 2xl:hidden" />
+  <div class="flex flex-1">
+    <LayoutMenuSidebar class="max-lg:hidden 2xl:hidden" />
     <div class="flex min-w-0 flex-1 flex-col">
       <div
-        class="container mx-auto flex flex-1 shrink flex-col p-4 md:px-8 lg:block lg:overflow-y-auto"
+        class="container flex flex-1 flex-col gap-4 self-center overflow-hidden p-4 md:px-8 2xl:gap-8"
       >
-        <div class="flex-1 2xl:pb-32">
-          <LayoutHeader />
-          <main class="flex-1 overflow-hidden p-1">
-            <slot />
-          </main>
-        </div>
+        <LayoutHeader />
+        <main>
+          <slot />
+        </main>
       </div>
       <LayoutMenuBottomNavigation class="lg:hidden" />
     </div>
