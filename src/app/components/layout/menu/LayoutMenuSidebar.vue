7b7f890feecca478c97f2582a142a7fefcb1b14f
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-04-10 09:38:59 +0200

    fix(components): make navigations accessible

--- src/app/components/layout/menu/LayoutMenuSidebar.vue
+++ src/app/components/layout/menu/LayoutMenuSidebar.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex max-w-1/4 flex-col">
     <nav
+      :aria-label="t('nav')"
       :class="
         cn(
           'sticky top-0 flex h-screen flex-col gap-4 border-r-1 border-(--semantic-base-line) bg-(--semantic-base-background) p-8 shadow-xs',
@@ -18,7 +19,16 @@ import type { HtmlHTMLAttributes } from 'vue'
 
 import { cn } from '@/utils/shadcn'
 
+const { t } = useI18n()
+
 const { class: classProps } = defineProps<{
   class?: HtmlHTMLAttributes['class']
 }>()
 </script>
+
+<i18n lang="yaml">
+de:
+  nav: Seitenleiste
+en:
+  nav: Sidebar
+</i18n>
