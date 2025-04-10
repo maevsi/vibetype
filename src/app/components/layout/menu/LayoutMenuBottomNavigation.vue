cf2f81b8184c7a5f455aa7d582763a5d65145ee2
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-04-10 09:38:59 +0200

    fix(components): make navigations accessible

--- src/app/components/layout/menu/LayoutMenuBottomNavigation.vue
+++ src/app/components/layout/menu/LayoutMenuBottomNavigation.vue
@@ -1,5 +1,6 @@
 <template>
   <nav
+    :aria-label="t('nav')"
     :class="
       cn(
         'sticky bottom-0 flex items-center justify-center gap-1 border-t-1 border-(--semantic-base-line) bg-(--semantic-base-background) px-2 py-2 shadow-xs',
@@ -19,4 +20,13 @@ import { cn } from '@/utils/shadcn'
 const { class: classProps } = defineProps<{
   class?: HtmlHTMLAttributes['class']
 }>()
+
+const { t } = useI18n()
 </script>
+
+<i18n lang="yaml">
+de:
+  nav: Untere
+en:
+  nav: Bottom
+</i18n>
