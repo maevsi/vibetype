06584efd43bbd47016e3d9e51962f0da6a5098fb
huzaifaedhi2 <huzaifaejaz09@gmail.com>
2025-04-21 21:26:45 +0400

    feat(error-view): implement error view component

---
+++ src/app/components/error/View.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="flex flex-1 flex-col gap-10">
+    <LayoutPageContent>
+      <LayoutPageResult type="error">
+        <template #description>{{ t('description') }}</template>
+      </LayoutPageResult>
+      <template #bottom-navigation-extension>
+        <ButtonColored
+          :aria-label="t('backToLogin')"
+          class="w-full max-w-sm"
+          :to="localePath('session-create')"
+          variant="primary-critical"
+          @click="emit('close')"
+        >
+          {{ t('backToLogin') }}
+        </ButtonColored>
+      </template>
+    </LayoutPageContent>
+  </section>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default-no-header',
+})
+const emit = defineEmits<{
+  close: []
+}>()
+const localePath = useLocalePath()
+const { t } = useI18n()
+</script>
+
+<i18n lang="yaml">
+de:
+  description: Bitte versuche es erneut
+  backToLogin: Zurück zur Registrierung
+en:
+  description: Please try again
+  backToLogin: Back to Login
+</i18n>
