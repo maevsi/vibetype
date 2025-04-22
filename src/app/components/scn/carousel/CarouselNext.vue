d73e220891b3f5bdd4c9236891e05fb89921261e
huzaifaedhi2 <huzaifaejaz09@gmail.com>
2025-04-22 16:47:46 +0400

    chore(shadcn): add common shad cn components

---
+++ src/app/components/scn/carousel/CarouselNext.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import type { WithClassAsProps } from './interface'
+import { cn } from '@/utils/shadcn'
+import { ShadButton, type ButtonVariants } from '@/components/scn/button'
+import { ArrowRight } from 'lucide-vue-next'
+import { useCarousel } from './useCarousel'
+
+const props = withDefaults(
+  defineProps<
+    {
+      variant?: ButtonVariants['variant']
+      size?: ButtonVariants['size']
+    } & WithClassAsProps
+  >(),
+  {
+    variant: 'outline',
+    size: 'icon',
+  },
+)
+
+const { orientation, canScrollNext, scrollNext } = useCarousel()
+</script>
+
+<template>
+  <ShadButton
+    data-slot="carousel-next"
+    :disabled="!canScrollNext"
+    :class="
+      cn(
+        'absolute size-8 rounded-full',
+        orientation === 'horizontal'
+          ? 'top-1/2 -right-12 -translate-y-1/2'
+          : '-bottom-12 left-1/2 -translate-x-1/2 rotate-90',
+        props.class,
+      )
+    "
+    :variant="variant"
+    :size="size"
+    @click="scrollNext"
+  >
+    <slot>
+      <ArrowRight />
+    </slot>
+  </ShadButton>
+</template>
---
+++ src/app/components/scn/carousel/CarouselPrevious.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import type { WithClassAsProps } from './interface'
+import { cn } from '@/utils/shadcn'
+import { ShadButton, type ButtonVariants } from '@/components/scn/button'
+import { ArrowLeft } from 'lucide-vue-next'
+import { useCarousel } from './useCarousel'
+
+const props = withDefaults(
+  defineProps<
+    {
+      variant?: ButtonVariants['variant']
+      size?: ButtonVariants['size']
+    } & WithClassAsProps
+  >(),
+  {
+    variant: 'outline',
+    size: 'icon',
+  },
+)
+
+const { orientation, canScrollPrev, scrollPrev } = useCarousel()
+</script>
+
+<template>
+  <ShadButton
+    data-slot="carousel-previous"
+    :disabled="!canScrollPrev"
+    :class="
+      cn(
+        'absolute size-8 rounded-full',
+        orientation === 'horizontal'
+          ? 'top-1/2 -left-12 -translate-y-1/2'
+          : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
+        props.class,
+      )
+    "
+    :variant="variant"
+    :size="size"
+    @click="scrollPrev"
+  >
+    <slot>
+      <ArrowLeft />
+    </slot>
+  </ShadButton>
+</template>
