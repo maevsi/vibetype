b2818f5f7fde5357687f2ee5d73beb6007b7ce4d
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-04-25 00:04:10 +0200

    fix(layout): show sidebar on all large screens

--- src/app/layouts/default.vue
+++ src/app/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-1">
-    <LayoutMenuSidebar class="max-lg:hidden 2xl:hidden" />
+    <LayoutMenuSidebar class="max-lg:hidden" />
     <div class="flex min-w-0 flex-1 flex-col">
       <div
         class="container flex flex-1 flex-col gap-4 self-center overflow-hidden p-4 md:px-8 2xl:gap-8"
