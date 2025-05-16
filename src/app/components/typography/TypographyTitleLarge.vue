ba02b505aaa6992a1e7846e01a5b0e40f7d0ba74
huzaifaedhi2 <huzaifaejaz09@gmail.com>
2025-05-16 16:26:49 +0200

    chore(styling): add color and typography style

---
+++ src/app/components/typography/TypographyTitleLarge.vue
@@ -0,0 +1,15 @@
+<template>
+  <span :class="cn('text-large font-semibold', classProps)">
+    <slot />
+  </span>
+</template>
+
+<script setup lang="ts">
+import type { HtmlHTMLAttributes } from 'vue'
+
+import { cn } from '@/utils/shadcn'
+
+const { class: classProps } = defineProps<{
+  class?: HtmlHTMLAttributes['class']
+}>()
+</script>
