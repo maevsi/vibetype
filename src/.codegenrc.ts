c256e0b81f787f2d8edc72fb5ec9d88a69848559
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-05-20 08:42:20 +0200

    chore(gql): generate type imports for all files

--- src/.codegenrc.ts
+++ src/.codegenrc.ts
@@ -2,15 +2,14 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 
 export default {
   schema: 'https://postgraphile.localhost/graphql',
-  documents: ['gql/documents/**/*.ts'],
+  documents: ['gql/documents/**/*.ts'], // ignoreNoDocuments: true,
   hooks: { afterAllFileWrite: ['prettier --write', 'eslint --fix'] },
-  // ignoreNoDocuments: true,
+  config: {
+    useTypeImports: true,
+  },
   generates: {
     './gql/generated/': {
       preset: 'client',
-      config: {
-        useTypeImports: true,
-      },
     },
     'gql/generated/graphcache.ts': {
       plugins: [
