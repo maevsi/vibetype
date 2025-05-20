864a1005b5d2c6d2906ab637db66b6b627753d0f
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-05-20 09:31:19 +0200

    refactor: align variable names

--- src/shared/utils/schema.ts
+++ src/shared/utils/schema.ts
@@ -1,13 +1,13 @@
 import { z } from 'zod'
 
 export const schemaFormContact = z.object({
-  consent: z.boolean().refine((val) => val === true),
+  consent: z.boolean().refine((value) => value === true),
   emailAddress: z.string().email().max(1000),
   name: z.string().max(100),
   message: z.string().max(10000),
 })
 export const schemaFormEarlyBird = z.object({
-  agreement: z.boolean().refine((val) => val === true),
+  agreement: z.boolean().refine((value) => value === true),
   emailAddress: z.string().email().max(1000),
   name: z.string().max(100),
 })
