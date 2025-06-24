89b216bc495a880f20efd78d2836f95bdc3d268b
huzaifaedhi2 <huzaifaejaz09@gmail.com>
2025-06-24 19:41:00 +0200

    fix: add config to nuxt.ts

--- src/.config/nuxt.ts
+++ src/.config/nuxt.ts
@@ -171,6 +171,28 @@ export default defineNuxtConfig({
             id: undefined,
             groupId: undefined,
           },
+          issue: {
+            column: {
+              consentId: undefined,
+              descriptionId: undefined,
+              emailId: undefined,
+              issueNameId: undefined,
+              requestorId: undefined,
+            },
+            id: undefined,
+            groupId: undefined,
+          },
+          report: {
+            column: {
+              accuracyConfirmationId: undefined,
+              emailAddressId: undefined,
+              messageId: undefined,
+              privacyConsentId: undefined,
+              reportNameId: undefined,
+            },
+            id: undefined,
+            groupId: undefined,
+          },
         },
       },
       openai: {
