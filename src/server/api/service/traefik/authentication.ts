1a7c3d164e1a7157e75dbd8a89db6e8767e015ac
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-07-05 03:34:30 +0200

    perf(api): link mutation names for verification

--- src/server/api/service/traefik/authentication.ts
+++ src/server/api/service/traefik/authentication.ts
@@ -3,6 +3,9 @@ import { parse } from 'graphql'
 import type { H3Event } from 'h3'
 import { z } from 'zod'
 
+import { authenticateMutation } from '~~/gql/documents/mutations/account/accountAuthenticate'
+import { accountRegistrationMutation } from '~~/gql/documents/mutations/account/accountRegistration'
+
 const authProxyBodySchema = z.object({
   query: z.string().optional(),
   variables: z
@@ -29,18 +32,35 @@ export default defineEventHandler(async (event) => {
 
   const ast = parse(body.query)
 
+  if (
+    // authentication
+    authenticateMutation.definitions.length !== 1 ||
+    !authenticateMutation.definitions[0] ||
+    !('name' in authenticateMutation.definitions[0]) ||
+    !authenticateMutation.definitions[0].name ||
+    // account registration
+    accountRegistrationMutation.definitions.length !== 1 ||
+    !accountRegistrationMutation.definitions[0] ||
+    !('name' in accountRegistrationMutation.definitions[0]) ||
+    !accountRegistrationMutation.definitions[0].name
+  )
+    return throwError({
+      code: 500,
+      message: 'Could not get name of mutation.',
+    })
+
   for (const definition of ast.definitions) {
     if (definition.kind !== 'OperationDefinition' || !definition.name) continue
 
     switch (definition.name.value) {
-      case 'authenticate':
+      case authenticateMutation.definitions[0].name.value:
         // don't check captcha for anonymous authentication
         if (body.variables?.password === '' && body.variables.username === '')
           return
 
         await turnstileVerify(event)
         break
-      case 'accountRegistration':
+      case accountRegistrationMutation.definitions[0].name.value:
         await turnstileVerify(event)
         break
       default:
