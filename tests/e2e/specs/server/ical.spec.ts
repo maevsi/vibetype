559b4db5d8cd1ab8027843e3eb9245aad65e1061
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-07-30 05:43:29 +0200

    test(e2e): reenable ical post test

--- tests/e2e/specs/server/ical.spec.ts
+++ tests/e2e/specs/server/ical.spec.ts
@@ -1,18 +1,17 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, type APIResponse } from '@playwright/test'
 
 test.describe('api load', () => {
-  // TODO: fix linting
-  // test('only allows POST requests', async ({ request }) => {
-  //   const disallowedMethods = ['get', 'put', 'delete', 'patch']
+  test('only allows POST requests', async ({ request }) => {
+    const disallowedMethods = ['get', 'put', 'delete', 'patch']
 
-  //   for (const disallowedMethod of disallowedMethods) {
-  //     const resp = await (
-  //       request[disallowedMethod] as (url: string) => Promise<APIResponse>
-  //     )('/api/model/event/ical')
-  //     expect(resp.status()).toEqual(405)
-  //     expect(resp.statusText()).toEqual('Only POST requests are allowed!')
-  //   }
-  // })
+    for (const disallowedMethod of disallowedMethods) {
+      const resp = await (
+        request[disallowedMethod] as (url: string) => Promise<APIResponse>
+      )('/api/model/event/ical')
+      expect(resp.status()).toEqual(404)
+      expect(resp.statusText()).toEqual('Page not found: /api/model/event/ical')
+    }
+  })
 
   test('validates input data', async ({ request }) => {
     const inputData = [
