e0f1dd438400001144f9bdf73491533e8e8c06be
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-11-25 07:17:06 +0100

    refactor(components): use nuxt time properties type

--- src/app/components/app/AppTime.vue
+++ src/app/components/app/AppTime.vue
@@ -10,46 +10,24 @@
 import { reactiveOmit } from '@vueuse/core'
 import { useForwardProps } from 'reka-ui'
 
-// TODO: use imported type (https://github.com/nuxt/nuxt/issues/29757)
-// import type { NuxtTimeProps } from '#app'
+import type { NuxtTimeProps } from '#app'
 
 const { locale: defaultLocale } = useI18n()
 const defaultTimeZone = useTimeZone()
 
-const props = withDefaults(
-  defineProps</*NuxtTimeProps*/ {
-    datetime: string | number | Date
-    day?: 'numeric' | '2-digit'
-    hour?: 'numeric' | '2-digit'
-    locale?: string
-    minute?: 'numeric' | '2-digit'
-    month?: 'numeric' | '2-digit' | 'long' | 'short' | 'narrow'
-    relative?: boolean
-    timeZone?: string
-    timeZoneName?:
-      | 'short'
-      | 'long'
-      | 'shortOffset'
-      | 'longOffset'
-      | 'shortGeneric'
-      | 'longGeneric'
-    weekday?: 'long' | 'short' | 'narrow'
-    year?: 'numeric' | '2-digit'
-  }>(),
-  {
-    // ...dateTimeFormatOptions, TODO: use shared options
-    day: 'numeric',
-    hour: 'numeric',
-    locale: undefined,
-    minute: 'numeric',
-    month: 'short',
-    relative: undefined,
-    timeZone: undefined,
-    timeZoneName: undefined,
-    weekday: undefined,
-    year: 'numeric',
-  },
-)
+const props = withDefaults(defineProps<NuxtTimeProps>(), {
+  // ...dateTimeFormatOptions, TODO: use shared options
+  day: 'numeric',
+  hour: 'numeric',
+  locale: undefined,
+  minute: 'numeric',
+  month: 'short',
+  relative: undefined,
+  timeZone: undefined,
+  timeZoneName: undefined,
+  weekday: undefined,
+  year: 'numeric',
+})
 
 const delegatedProps = reactiveOmit(props, 'locale', 'timeZone')
 const forwardedProps = useForwardProps(delegatedProps)
