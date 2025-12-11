a81a97ff96e2eadad7d00fb4f61d0368877811e5
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-12-11 04:54:50 +0100

    chore(reccoom): disable in frontend-only development

--- src/server/api/service/reccoom/recommendations.get.ts
+++ src/server/api/service/reccoom/recommendations.get.ts
@@ -4,6 +4,7 @@ export default defineEventHandler(async () => {
   const jwt = getJwtFromCookie()
   const jwtDecoded = await verifyJwt(jwt)
   const getServiceHref = useGetServiceHref()
+  const runtimeConfig = useRuntimeConfig()
 
   if (!(jwtDecoded.role === `${SITE_NAME}_account`))
     return throwError({
@@ -11,8 +12,14 @@ export default defineEventHandler(async () => {
       statusMessage: 'This endpoint only available to registered users.',
     })
 
+  if (runtimeConfig.public.vio.stagingHost) {
+    // reccoom is currently not available in frontend-only development
+    return []
+  }
+
+  const baseURL = getServiceHref({ name: 'reccoom', port: 5245 })
   const recommendations = await $fetch<string[]>('/recommendations', {
-    baseURL: getServiceHref({ name: 'reccoom', port: 5245 }),
+    baseURL,
     headers: {
       cookie: `jwt=${jwt}`,
     },
