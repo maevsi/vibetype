91254f22ca0785c131dd2b1c5f7d195227bd6733
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-01-05 10:52:48 +0100

    refactor(nuxt): clarify development server https option setter

--- src/config/environments/development.ts
+++ src/config/environments/development.ts
@@ -1,6 +1,6 @@
 import type { DefineNuxtConfig } from 'nuxt/config'
 
-import { SITE_NAME } from '../../node/static'
+import { IS_IN_STACK, SITE_NAME } from '../../node/static'
 
 const HTTPS = {
   key: './.config/certificates/ssl.key',
@@ -9,13 +9,13 @@ const HTTPS = {
 
 export const developmentConfig: ReturnType<DefineNuxtConfig> = {
   $development: {
-    ...(process.env.NUXT_PUBLIC_SITE_URL // TODO: make more readable, find better naming ("enable https only in standalone mode, not when running inside the stack")
-      ? {}
-      : {
-          devServer: {
+    devServer: {
+      ...(IS_IN_STACK
+        ? {}
+        : {
             https: HTTPS,
-          },
-        }),
+          }),
+    },
     devtools: {
       enabled: !process.env.NUXT_PUBLIC_VIO_IS_TESTING,
     },
