0b1d28c4191a7aab4c362cca9d2d3c7bb49602e1
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-04-27 18:37:30 +0200

    perf(i18n): compact routes

--- src/config/modules/i18n.ts
+++ src/config/modules/i18n.ts
@@ -4,6 +4,9 @@ export const i18nConfig: ReturnType<DefineNuxtConfig> = {
   i18n: {
     defaultLocale: 'en', // Must be set for the default prefix_except_default prefix strategy.
     detectBrowserLanguage: false,
+    experimental: {
+      compactRoutes: true,
+    },
     locales: [
       {
         code: 'de',
