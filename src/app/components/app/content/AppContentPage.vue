80567a3d1ce0f793226e8d90d20c01d15ad05471
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-06-07 21:28:42 +0200

    chore(content): reenable open graph image

--- src/app/components/app/content/AppContentPage.vue
+++ src/app/components/app/content/AppContentPage.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup lang="ts">
-// import type { OgImageComponents } from '#og-image/components'
+import type { OgImageComponents } from '#og-image/components'
 
 // compiler
 const { path } = defineProps<{
@@ -15,12 +15,11 @@ const content = await useContent(path)
 
 // seo
 useHeadDefault({ title: computed(() => content.data.value?.title) })
-// TODO: reenable (https://github.com/nuxt-modules/og-image/issues/622)
-// if (content.data.value?.ogImage?.component) {
-//   // TODO: ensure the content's og image does overrides the default from `useHeadDefault` properly
-//   defineOgImage(
-//     content.data.value.ogImage.component as keyof OgImageComponents,
-//     content.data.value.ogImage.props,
-//   )
-// }
+if (content.data.value?.ogImage?.component) {
+  // TODO: ensure the content's og image does overrides the default from `useHeadDefault` properly
+  defineOgImage(
+    content.data.value.ogImage.component as keyof OgImageComponents,
+    content.data.value.ogImage.props,
+  )
+}
 </script>
