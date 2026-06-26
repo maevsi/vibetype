9eb7e4efe1b94a489b94ccb7c1b2c92f53040531
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-06-26 15:31:09 +0200

    feat(app)!: drop outdated detection keys

--- src/server/middleware/platform.ts
+++ src/server/middleware/platform.ts
@@ -1,10 +1,7 @@
 export default defineEventHandler(async (event) => {
-  const appPlatformHeader =
-    getHeader(event, `${SITE_NAME}_platform`) ||
-    getHeader(event, `${SITE_NAME}-platform`) // currently in use by maevsi/android
-  const appPlatformCookie =
-    getCookie(event, `${SITE_NAME}_platform`) ||
-    getCookie(event, 'app-platform') // currently in use by maevsi/ios
+  const key = `${SITE_NAME}_platform`
+  const appPlatformHeader = getHeader(event, key)
+  const appPlatformCookie = getCookie(event, key)
 
   event.context.$platform = appPlatformHeader || appPlatformCookie
 })
