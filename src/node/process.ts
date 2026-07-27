ae0f6057b30c972f54071a98bf1a4aaea6971454
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-07-27 02:05:49 +0200

    chore: test 3

--- src/node/process.ts
+++ src/node/process.ts
@@ -1,1 +1,8 @@
-export const RELEASE_NAME = async () => '0.0.0'
+import { exec } from 'node:child_process'
+import { promisify } from 'node:util'
+
+const execPromise = promisify(exec)
+
+export const RELEASE_NAME = async () =>
+  process.env.RELEASE_NAME ||
+  (await execPromise('git describe --tags')).stdout.trim()
