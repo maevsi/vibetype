fba3bad175416444674cc5788fcdbf4b6458a32a
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-08-05 05:12:50 +0200

    feat(error): propagate from components to pages

--- src/app/components/app/content/AppContent.vue
+++ src/app/components/app/content/AppContent.vue
@@ -1,15 +1,5 @@
 <template>
-  <LoaderIndicatorPing v-if="content.pending.value" />
-  <!-- TODO: use v-model error / error boundary -->
-  <AppError
-    v-else-if="content.error.value"
-    :error="{ message: content.error.value.message, status: 500 }"
-  />
-  <AppError
-    v-else-if="!content.data.value"
-    :error="{ message: 'Data is missing', status: 404 }"
-  />
-  <LayoutProse v-else>
+  <LayoutProse v-if="content.data.value">
     <ContentRenderer :value="content.data.value" />
   </LayoutProse>
 </template>
--- src/app/pages/docs/legal/terms/index.vue
+++ src/app/pages/docs/legal/terms/index.vue
@@ -1,3 +1,8 @@
 <template>
-  <ContentLegalTerms />
+  <AppError v-if="error" :error="{ message: error.message, status: 500 }" />
+  <ContentLegalTerms v-else v-model:error="error" />
 </template>
+
+<script setup lang="ts">
+const error = ref<Error | undefined>()
+</script>
