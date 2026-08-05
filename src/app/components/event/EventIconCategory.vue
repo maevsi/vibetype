f0106ba087c0f9bfd1a5908117f981b6ec858012
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-08-05 20:41:00 +0200

    feat(event): add category and format icon components

---
+++ src/app/components/event/EventIconCategory.vue
@@ -0,0 +1,98 @@
+<template>
+  <AppIconPreferenceCategoryArtAndCulture
+    v-if="name === 'art-and-culture'"
+    :class="classProps"
+    :title="t('artAndCulture')"
+  />
+  <AppIconPreferenceCategoryBusiness
+    v-else-if="name === 'business'"
+    :class="classProps"
+    :title="t('business')"
+  />
+  <AppIconPreferenceCategoryComedy
+    v-else-if="name === 'comedy'"
+    :class="classProps"
+    :title="t('comedy')"
+  />
+  <AppIconPreferenceCategoryEducation
+    v-else-if="name === 'education'"
+    :class="classProps"
+    :title="t('education')"
+  />
+  <AppIconPreferenceCategoryFashionAndLifestyle
+    v-else-if="name === 'fashion-and-lifestyle'"
+    :class="classProps"
+    :title="t('fashionAndLifestyle')"
+  />
+  <AppIconPreferenceCategoryFoodAndDrink
+    v-else-if="name === 'food-and-drink'"
+    :class="classProps"
+    :title="t('foodAndDrink')"
+  />
+  <AppIconPreferenceCategoryLiterature
+    v-else-if="name === 'literature'"
+    :class="classProps"
+    :title="t('literature')"
+  />
+  <AppIconPreferenceCategoryMusicAndEntertainment
+    v-else-if="name === 'music-and-entertainment'"
+    :class="classProps"
+    :title="t('musicAndEntertainment')"
+  />
+  <AppIconPreferenceCategoryPolitics
+    v-else-if="name === 'politics'"
+    :class="classProps"
+    :title="t('politics')"
+  />
+  <AppIconPreferenceCategorySocial
+    v-else-if="name === 'social'"
+    :class="classProps"
+    :title="t('social')"
+  />
+  <AppIconPreferenceCategorySportsAndFitness
+    v-else-if="name === 'sports-and-fitness'"
+    :class="classProps"
+    :title="t('sportsAndFitness')"
+  />
+  <AppIconPreferenceOther v-else :class="classProps" :title="t('other')" />
+</template>
+
+<script setup lang="ts">
+import type { HtmlHTMLAttributes } from 'vue'
+
+const { class: classProps = undefined, name } = defineProps<{
+  class?: HtmlHTMLAttributes['class']
+  name: string
+}>()
+
+const { t } = useI18n()
+</script>
+
+<i18n lang="yaml">
+de:
+  artAndCulture: Kunst & Kultur
+  business: Wirtschaft
+  comedy: Komödie
+  education: Bildung
+  fashionAndLifestyle: Mode & Lifestyle
+  foodAndDrink: Essen & Trinken
+  literature: Literatur
+  musicAndEntertainment: Musik & Unterhaltung
+  other: Andere
+  politics: Politik
+  social: Soziales
+  sportsAndFitness: Sport & Fitness
+en:
+  artAndCulture: Art & Culture
+  business: Business
+  comedy: Comedy
+  education: Education
+  fashionAndLifestyle: Fashion & Lifestyle
+  foodAndDrink: Food & Drink
+  literature: Literature
+  musicAndEntertainment: Music & Entertainment
+  other: Other
+  politics: Politics
+  social: Social
+  sportsAndFitness: Sports & Fitness
+</i18n>
---
+++ src/app/components/event/EventIconFormat.vue
@@ -0,0 +1,98 @@
+<template>
+  <AppIconPreferenceFormatConference
+    v-if="name === 'conference'"
+    :class="classProps"
+    :title="t('conference')"
+  />
+  <AppIconPreferenceFormatDemonstration
+    v-else-if="name === 'demo'"
+    :class="classProps"
+    :title="t('demonstration')"
+  />
+  <AppIconPreferenceFormatExhibition
+    v-else-if="name === 'exhibition'"
+    :class="classProps"
+    :title="t('exhibition')"
+  />
+  <AppIconPreferenceFormatFestival
+    v-else-if="name === 'festival'"
+    :class="classProps"
+    :title="t('festival')"
+  />
+  <AppIconPreferenceFormatHackathon
+    v-else-if="name === 'hackathon'"
+    :class="classProps"
+    :title="t('hackathon')"
+  />
+  <AppIconPreferenceFormatLecture
+    v-else-if="name === 'lecture'"
+    :class="classProps"
+    :title="t('lecture')"
+  />
+  <AppIconPreferenceFormatLivePerformance
+    v-else-if="name === 'live-performance'"
+    :class="classProps"
+    :title="t('livePerformance')"
+  />
+  <AppIconPreferenceFormatMeetup
+    v-else-if="name === 'meetup'"
+    :class="classProps"
+    :title="t('meetup')"
+  />
+  <AppIconPreferenceFormatParty
+    v-else-if="name === 'party'"
+    :class="classProps"
+    :title="t('party')"
+  />
+  <AppIconPreferenceFormatSeminar
+    v-else-if="name === 'seminar'"
+    :class="classProps"
+    :title="t('seminar')"
+  />
+  <AppIconPreferenceFormatWorkshop
+    v-else-if="name === 'workshop'"
+    :class="classProps"
+    :title="t('workshop')"
+  />
+  <AppIconPreferenceOther v-else :class="classProps" :title="t('other')" />
+</template>
+
+<script setup lang="ts">
+import type { HtmlHTMLAttributes } from 'vue'
+
+const { class: classProps = undefined, name } = defineProps<{
+  class?: HtmlHTMLAttributes['class']
+  name: string
+}>()
+
+const { t } = useI18n()
+</script>
+
+<i18n lang="yaml">
+de:
+  conference: Konferenz
+  demonstration: Demonstration
+  exhibition: Ausstellung
+  festival: Festival
+  hackathon: Hackathon
+  lecture: Vortrag
+  livePerformance: Live-Auftritt
+  meetup: Meetup
+  other: Andere
+  party: Party
+  seminar: Seminar
+  workshop: Workshop
+en:
+  conference: Conference
+  demonstration: Demonstration
+  exhibition: Exhibition
+  festival: Festival
+  hackathon: Hackathon
+  lecture: Lecture
+  livePerformance: Live Performance
+  meetup: Meetup
+  other: Other
+  party: Party
+  seminar: Seminar
+  workshop: Workshop
+</i18n>
