fbaf70836a87fdc6bad86b94b63485bda67a710b
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-08-11 01:46:50 +0200

    style(auth): restyle FormAuthInput with figma tokens

--- src/app/components/form/auth/FormAuthInput.vue
+++ src/app/components/form/auth/FormAuthInput.vue
@@ -2,32 +2,28 @@
   <div class="relative">
     <div
       v-if="$slots.icon"
-      class="pointer-events-none absolute top-1/2 left-4 size-5 -translate-y-1/2 text-gray-500 dark:text-gray-400"
+      class="pointer-events-none absolute top-1/2 left-4 size-5 -translate-y-1/2 text-(--figma-neutral-level-6)"
     >
       <slot name="icon" />
     </div>
-    <Input
-      :id
-      :aria-invalid="ariaInvalid"
-      :class="[
-        cn(
-          'h-13 rounded-2xl border-transparent bg-gray-100 pl-12 text-[17px] dark:bg-gray-800',
-          { 'border-red-500': ariaInvalid },
-        ),
-      ]"
-      :disabled
-      :model-value="modelValue"
-      :placeholder
-      :type
-      @blur="emit('blur')"
-      @input="emit('input', ($event.target as HTMLInputElement).value)"
-    />
+    <AppTypographySubheadline v-slot="attributes" variant="emphasized">
+      <Input
+        v-bind="attributes"
+        :id
+        :aria-invalid
+        class="rounded-(--default-border-radius) bg-(--figma-neutral-level-1) px-6 py-4 pl-12 text-[17px] aria-invalid:border-(--figma-critic-red-middle-dark)"
+        :disabled
+        :model-value
+        :placeholder
+        :type
+        @blur="emit('blur')"
+        @input="emit('input', ($event.target as HTMLInputElement).value)"
+      />
+    </AppTypographySubheadline>
   </div>
 </template>
 
 <script setup lang="ts">
-import { cn } from '@/utils/shadcn'
-
 defineProps<{
   ariaInvalid?: boolean
   disabled?: boolean
