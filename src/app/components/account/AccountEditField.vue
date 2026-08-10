2991f772ad4fbaa81e0054ebd9f1e9810595b82f
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-08-11 01:58:20 +0200

    feat(account): add edit field component

---
+++ src/app/components/account/AccountEditField.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col gap-2 px-2">
+    <p class="text-sm font-semibold">
+      <slot name="label">{{ label }}</slot>
+    </p>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ label?: string }>()
+</script>
