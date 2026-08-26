2ffb1e5f22051eec657158e3c52a076a1acc43d0
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-08-26 02:46:46 +0200

    feat(event): drop templating from descriptions (#2387)

--- src/server/api/model/event/ical.post.ts
+++ src/server/api/model/event/ical.post.ts
@@ -4,12 +4,6 @@ import { EventVisibility } from '~~/gql/generated/graphcache'
 import { SITE_URL } from '~~/node/static'
 
 const icalPostBodySchema = z.object({
-  contact: z
-    .object({
-      firstName: z.string().nullable(),
-      lastName: z.string().nullable(),
-    })
-    .optional(),
   event: z.object({
     accountByCreatedBy: z.object({
       username: z.string(),
@@ -32,7 +26,6 @@ const icalPostBodySchema = z.object({
 export default defineEventHandler(async (h3Event) => {
   const body = await getBodySafe({ event: h3Event, schema: icalPostBodySchema })
 
-  const contact = body.contact
   const event = body.event
   const eventAuthorUsername = body.event.accountByCreatedBy.username
   const guest = body.guest
@@ -42,5 +35,5 @@ export default defineEventHandler(async (h3Event) => {
     'Content-Disposition': `attachment; filename="${eventAuthorUsername}_${event.slug}.ics"`,
   })
 
-  return getIcalString({ contact, event, guest, siteUrl: SITE_URL })
+  return getIcalString({ event, guest, siteUrl: SITE_URL })
 })
